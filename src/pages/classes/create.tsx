6716a46f67fb3ab7386420e93c33157743be1b01
Travis <travis@vandoozer.com>
2026-09-02 17:31:19 -0600

    finalize create classes form

--- src/pages/classes/create.tsx
+++ src/pages/classes/create.tsx
@@ -45,6 +45,7 @@ const Create = () => {
   });
 
   const {
+    refineCore: { onFinish },
     handleSubmit,
     formState: { isSubmitting, errors },
     control,
@@ -52,49 +53,32 @@ const Create = () => {
 
   const onSubmit = async (values: z.infer<typeof classSchema>) => {
     try {
-      console.log(values);
+      await onFinish(values);
     } catch (error) {
       console.error("Error creating class:", error);
     }
   };
 
-  const teachers = [
-    {
-      id: "1",
-      name: "John Doe",
+  const { query: subjectsQuery } = useList<Subject>({
+    resource: "subjects",
+    pagination: {
+      pageSize: 100,
     },
-    {
-      id: "2",
-      name: "Jane Smith",
-    },
-    {
-      id: "3",
-      name: "Dr. Alan Turing",
-    },
-  ];
+  });
 
-  const subjects = [
-    {
-      id: 1,
-      name: "Mathematics",
-      code: "MATH",
+  const { query: teachersQuery } = useList<User>({
+    resource: "users",
+    filters: [{ field: "role", operator: "eq", value: "teacher" }],
+    pagination: {
+      pageSize: 100,
     },
-    {
-      id: 2,
-      name: "Computer Science",
-      code: "CS",
-    },
-    {
-      id: 3,
-      name: "Physics",
-      code: "PHY",
-    },
-    {
-      id: 4,
-      name: "Chemistry",
-      code: "CHEM",
-    },
-  ];
+  });
+
+  const subjects = subjectsQuery?.data?.data || [];
+  const subjectsLoading = subjectsQuery.isLoading;
+
+  const teachers = teachersQuery?.data?.data || [];
+  const teachersLoading = teachersQuery.isLoading;
 
   const bannerPublicId = form.watch("bannerCldPubId");
 
@@ -194,6 +178,7 @@ const Create = () => {
                         <Select
                           onValueChange={(value) => field.onChange(Number(value))}
                           value={field?.value?.toString()}
+                          disabled={subjectsLoading}
                         >
                           <FormControl>
                             <SelectTrigger className="w-full">
@@ -222,8 +207,9 @@ const Create = () => {
                           Teacher <span className="text-red-700">*</span>
                         </FormLabel>
                         <Select
-                          onValueChange={(value) => field.onChange(Number(value))}
+                          onValueChange={field.onChange}
                           value={field?.value?.toString()}
+                          disabled={teachersLoading}
                         >
                           <FormControl>
                             <SelectTrigger className="w-full">
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,6 +1,6 @@
 import { BACKEND_BASE_URL } from "@/constants";
-import { ListResponse } from "@/types";
-// import { HttpError } from "@refinedev/core";
+import { CreateResponse, ListResponse } from "@/types";
+import { HttpError } from "@refinedev/core";
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 
 if (!BACKEND_BASE_URL) {
@@ -64,6 +64,18 @@ const options: CreateDataProviderOptions = {
       const payload: ListResponse = await response.clone().json();
 
       return payload.pagination?.total ?? payload.data?.length ?? 0;
+    },
+  },
+
+  create: {
+    getEndpoint: ({ resource }) => resource,
+
+    buildBodyParams: async ({ variables }) => variables,
+
+    mapResponse: async (response) => {
+      const json: CreateResponse = await response.json();
+
+      return json.data ?? [];
     },
   },
 };
